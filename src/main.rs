77e2f1f7d787baaa761cb4bed965fc0391a7989f
Ray <raiyan.ahmed@gmail.com>
2024-01-04 21:40:39 +1100

    Can scrape hrefs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use spooderman::Scraper;
 
-
 #[tokio::main]
 async fn main() {
-    let mut scraper = Scraper::new()
-        .set_url("https://timetable.unsw.edu.au/2024/subjectSearch.html".to_string());
-    
+    let mut scraper =
+        Scraper::new().set_url("https://timetable.unsw.edu.au/2024/subjectSearch.html".to_string());
+
     match scraper.run_scraper().await {
-        Ok(_res) => {println!("Scraping successful!\n");
-    },
+        Ok(_res) => {
+            println!("Scraping successful!\n");
+        }
         Err(e) => eprintln!("Error: {}", e),
     }
 }
