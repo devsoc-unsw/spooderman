8c601463e5084a940f5b8508e88bbe4f7b669e25
fritzrehde <fritzrehde.work@gmail.com>
2025-04-18 13:23:49 +1000

    add ratelimiter and concurrency wip

--- src/school_area_scraper.rs
+++ src/school_area_scraper.rs
@@ -1,68 +1,66 @@
 use crate::{
-    scraper::fetch_url,
-    subject_area_scraper::SubjectAreaScraper,
+    Course,
+    requests::RequestClient,
+    subject_area_scraper::SubjectArea,
     text_manipulators::{extract_text, extract_year, get_html_link_to_page},
 };
+use derive_new::new;
 use scraper::Selector;
 use std::sync::Arc;
-use tokio::sync::Mutex;
 
-#[derive(Debug)]
+#[derive(Debug, new)]
 pub struct SchoolAreaPage {
     pub course_code: String,
     pub course_name: String,
     pub school: String,
-    pub subject_area_scraper: Arc<Mutex<SubjectAreaScraper>>,
+    pub subject_area: SubjectArea,
 }
 
 #[derive(Debug)]
-pub struct SchoolAreaScraper {
+pub struct SchoolArea {
     pub url: String,
     pub pages: Vec<SchoolAreaPage>,
 }
 
-impl SchoolAreaScraper {
-    pub async fn scrape(url: String) -> anyhow::Result<Self> {
+impl SchoolArea {
+    pub async fn scrape(url: String, request_client: &Arc<RequestClient>) -> anyhow::Result<Self> {
         log::info!("Scraping School Area for: {}", url);
 
-        let html = fetch_url(&url)
-            .await
-            .expect("There has been something wrong with the URL.");
-        let row_selector = Selector::parse("tr.rowLowlight, tr.rowHighlight").unwrap();
-        let code_selector = Selector::parse("td.data").unwrap();
-
-        let name_selector = Selector::parse("td.data a").unwrap();
-        let link_selector = Selector::parse("td.data a").unwrap();
-
-        let school_selector = Selector::parse("td.data:nth-child(3)").unwrap();
+        let html = request_client.fetch_url(&url).await?;
         let document = scraper::Html::parse_document(&html);
 
-        let pages = document
-            .select(&row_selector)
-            .map(|row_node| {
-                // Extract data from each row
-                let course_code = extract_text(row_node.select(&code_selector).next().unwrap());
-                let course_name = extract_text(row_node.select(&name_selector).next().unwrap());
-                let url_to_scrape_further = get_html_link_to_page(
-                    extract_year(&url).unwrap() as i32,
-                    row_node
-                        .select(&link_selector)
-                        .next()
-                        .map_or("", |node| node.value().attr("href").unwrap_or("")),
-                );
+        let row_selector = Selector::parse("tr.rowLowlight, tr.rowHighlight").unwrap();
+        let code_selector = Selector::parse("td.data").unwrap();
+        let name_selector = Selector::parse("td.data a").unwrap();
+        let link_selector = Selector::parse("td.data a").unwrap();
+        let school_selector = Selector::parse("td.data:nth-child(3)").unwrap();
 
-                let school = extract_text(row_node.select(&school_selector).next().unwrap());
-                SchoolAreaPage {
-                    course_code,
-                    course_name,
-                    school,
-                    subject_area_scraper: Arc::new(Mutex::new(SubjectAreaScraper::new(
-                        url_to_scrape_further,
-                    ))),
-                }
-            })
-            .collect();
+        let mut pages = vec![];
+        for row_node in document.select(&row_selector) {
+            // Extract data from each row
+            let course_code = extract_text(row_node.select(&code_selector).next().unwrap());
+            let course_name = extract_text(row_node.select(&name_selector).next().unwrap());
+            let url_to_scrape_further = get_html_link_to_page(
+                extract_year(&url).unwrap() as i32,
+                row_node
+                    .select(&link_selector)
+                    .next()
+                    .map_or("", |node| node.value().attr("href").unwrap_or("")),
+            );
+            let school = extract_text(row_node.select(&school_selector).next().unwrap());
 
+            let subject_area = SubjectArea::scrape(url_to_scrape_further, request_client).await?;
+            let subject_area_page =
+                SchoolAreaPage::new(course_code, course_name, school, subject_area);
+            pages.push(subject_area_page);
+        }
         Ok(Self { url, pages })
+    }
+
+    pub fn get_all_courses(self) -> impl Iterator<Item = Course> {
+        self.pages
+            .into_iter()
+            .map(|school_area_page| school_area_page.subject_area.courses)
+            .flatten()
     }
 }
