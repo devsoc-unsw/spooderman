3c708522c8b953b2f44bfa6ed4e2a9e87b2a0928
fritzrehde <fritzrehde.work@gmail.com>
2025-08-11 16:31:30 +1000

    implemented linear request rate backoff

--- src/ratelimit.rs
+++ src/ratelimit.rs
@@ -5,36 +5,87 @@ use governor::{
     state::{InMemoryState, NotKeyed},
 };
 use nonzero_ext::nonzero;
-use std::{num::NonZeroU32, time::Duration};
+use std::{
+    fmt::{self},
+    num::NonZeroU32,
+    time::{Duration, Instant},
+};
+use tokio::sync::RwLock;
 
-// NOTE: need to experiment manually with values for which UNSW doesn't rate-limit us.
+use crate::Request;
+
 // The higher, the faster.
-const REQ_PER_SEC: NonZeroU32 = nonzero!(150u32);
+const DEFAULT_REQ_PER_SEC: NonZeroU32 = nonzero!(150u32);
+
 // The lower, the faster.
-const MS_BETWEEN_REQ: Duration = Duration::from_millis(2);
+const DEFAULT_MS_BETWEEN_REQ: Duration = Duration::from_millis(2);
+
+// The higher, the faster the backoff.
+const LINEAR_REQUEST_RATE_BACKOFF: NonZeroU32 = nonzero!(30u32);
+
+#[derive(Debug, Clone, Copy)]
+struct RequestRate {
+    req_per_sec: NonZeroU32,
+    ms_between_req: Duration,
+    most_recently_updated: Instant,
+}
+
+impl RequestRate {
+    fn new(
+        req_per_sec: NonZeroU32,
+        ms_between_req: Duration,
+        most_recently_updated: Instant,
+    ) -> Self {
+        Self {
+            req_per_sec,
+            ms_between_req,
+            most_recently_updated,
+        }
+    }
+}
+
+impl fmt::Display for RequestRate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} requests per second", self.req_per_sec)?;
+        Ok(())
+    }
+}
 
 type SpecificGovernorRateLimiter =
     GovernorRateLimiter<NotKeyed, InMemoryState, QuantaClock, NoOpMiddleware<QuantaInstant>>;
 
-pub struct RateLimiter {
-    req_per_sec: SpecificGovernorRateLimiter,
-    ms_between_req: SpecificGovernorRateLimiter,
+struct FixedRateLimiter {
+    request_rate: RequestRate,
+    req_per_sec_rate_limiter: SpecificGovernorRateLimiter,
+    ms_between_req_rate_limiter: SpecificGovernorRateLimiter,
 }
 
-impl RateLimiter {
-    pub fn new() -> Self {
+impl FixedRateLimiter {
+    fn new(request_rate: RequestRate) -> Self {
         // Limit to X total req/sec on average.
-        let req_per_sec = GovernorRateLimiter::direct(Quota::per_second(REQ_PER_SEC));
+        let req_per_sec_rate_limiter =
+            GovernorRateLimiter::direct(Quota::per_second(request_rate.req_per_sec));
 
         // Limit to Y req/ms (i.e. no two requests closer than Y ms).
-        let ms_between_req = GovernorRateLimiter::direct(
-            Quota::with_period(MS_BETWEEN_REQ).expect("hardcoded MS_BETWEEN_REQ is larger than 0"),
+        let ms_between_req_rate_limiter = GovernorRateLimiter::direct(
+            Quota::with_period(request_rate.ms_between_req)
+                .expect("millis between requests should be larger than 0"),
         );
 
-        RateLimiter {
-            req_per_sec,
-            ms_between_req,
+        Self {
+            request_rate,
+            req_per_sec_rate_limiter,
+            ms_between_req_rate_limiter,
         }
+    }
+
+    fn update_request_rate(&mut self, new_request_rate: RequestRate) {
+        log::info!(
+            "updating the request rate from '{}' to '{}'",
+            self.request_rate,
+            new_request_rate
+        );
+        *self = Self::new(new_request_rate);
     }
 
     pub async fn wait_until_ready(&self) {
@@ -51,8 +102,79 @@ impl RateLimiter {
         // check, which will be strict and only allow 1 caller to pass every Y ms.
 
         // We won't call this more than X times per sec.
-        self.req_per_sec.until_ready().await;
+        self.req_per_sec_rate_limiter.until_ready().await;
         // Y ms have passed since the last time we called this.
-        self.ms_between_req.until_ready().await;
+        self.ms_between_req_rate_limiter.until_ready().await;
+    }
+}
+
+pub struct RateLimiter {
+    fixed_rate_limiter: RwLock<FixedRateLimiter>,
+}
+
+fn sub_nonzero(a: NonZeroU32, b: NonZeroU32) -> Option<NonZeroU32> {
+    a.get().checked_sub(b.get()).and_then(NonZeroU32::new)
+}
+
+impl RateLimiter {
+    pub fn new() -> Self {
+        let request_rate =
+            RequestRate::new(DEFAULT_REQ_PER_SEC, DEFAULT_MS_BETWEEN_REQ, Instant::now());
+        let fixed_rate_limiter = RwLock::new(FixedRateLimiter::new(request_rate));
+        Self { fixed_rate_limiter }
+    }
+
+    pub async fn wait_until_ready(&self) {
+        let fixed_rate_limiter = self.fixed_rate_limiter.read().await;
+        fixed_rate_limiter.wait_until_ready().await;
+    }
+
+    pub async fn lower_request_rate(&self, failed_request: Request) -> anyhow::Result<()> {
+        let (most_recently_updated, old_request_rate) = {
+            let fixed_rate_limiter = self.fixed_rate_limiter.read().await;
+            (
+                fixed_rate_limiter.request_rate.most_recently_updated,
+                fixed_rate_limiter.request_rate,
+            )
+        };
+
+        // If the failed request (A) was made before the most recent request rate
+        // reduction (which was made due to failed request B), then A failing
+        // shouldn't lower the request rate even further, since we will have
+        // hundreds of failed requests like A, so reducing for all of them would cause
+        // our request rate to go to 0 immediately. Only the next failed request C
+        // after the latest request rate update should cause another request rate
+        // update, since the current request rate clearly isn't low enough at the point
+        // of C either.
+        if failed_request.sent_time <= most_recently_updated {
+            return Ok(());
+        }
+
+        {
+            let mut fixed_rate_limiter = self.fixed_rate_limiter.write().await;
+
+            let Some(new_req_per_sec) =
+                sub_nonzero(old_request_rate.req_per_sec, LINEAR_REQUEST_RATE_BACKOFF)
+            else {
+                let err_msg = "the request rate has been lowered to 0 req/sec so we can't lower it any further";
+                log::error!("{}", err_msg);
+                return Err(anyhow::anyhow!(err_msg));
+            };
+            let new_ms_beteen_reqs = old_request_rate.ms_between_req;
+            let new_request_rate =
+                RequestRate::new(new_req_per_sec, new_ms_beteen_reqs, Instant::now());
+
+            // Before switching to the new request rate, we should back off
+            // completely for a couple of seconds: kill all in-flight requests,
+            // and stop sending new requests for a couple of seconds.
+            // TODO
+
+            fixed_rate_limiter.update_request_rate(new_request_rate);
+        }
+
+        // log::info!("pausing requests for 1 second");
+        // tokio::time::sleep(Duration::from_secs(1)).await;
+
+        Ok(())
     }
 }
--- src/requests.rs
+++ src/requests.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use reqwest::{Client, ClientBuilder, Response};
 
 use crate::ratelimit::RateLimiter;
@@ -5,6 +7,18 @@ use crate::ratelimit::RateLimiter;
 pub struct RequestClient {
     client: Client,
     rate_limiter: RateLimiter,
+}
+
+pub struct Request {
+    /// The timestamp at which the client sent this request, regardless of when
+    /// the server responded and whether the server responded at all.
+    pub sent_time: Instant,
+}
+
+impl Request {
+    fn new(sent_time: Instant) -> Self {
+        Self { sent_time }
+    }
 }
 
 impl RequestClient {
@@ -20,12 +34,35 @@ impl RequestClient {
     }
 
     pub async fn fetch_url_response(&self, url: &str) -> anyhow::Result<Response> {
-        // Wait (non-blocking) until we're allowed to make a request according
-        // to our self-imposed rate-limiting policy.
-        self.rate_limiter.wait_until_ready().await;
+        loop {
+            // Wait (non-blocking) until we're allowed to make a request according
+            // to our self-imposed rate-limiting policy.
+            self.rate_limiter.wait_until_ready().await;
 
-        let response = self.client.get(url).send().await?;
-        Ok(response)
+            let request = Request::new(Instant::now());
+            match tokio::time::timeout(Duration::from_secs(3), self.client.get(url).send()).await {
+                Ok(Ok(response)) => return Ok(response),
+                Ok(Err(e)) => {
+                    log::warn!(
+                        "get-request to url {} failed ({}), so we'll reduce request rate",
+                        url,
+                        e
+                    );
+                }
+                Err(_) => {
+                    log::warn!(
+                        "get-request to url {} timed out (maybe UNSW servers are rate-limiting us by responding very slowly instead of returning an error), so we'll reduce request rate",
+                        url,
+                    );
+                }
+            }
+            let failed_request = request;
+
+            // If we got rate-limited by UNSW servers, we are probably making too
+            // many requests, so we should send requests at a lower rate (i.e.
+            // rate-limit ourselves more).
+            self.rate_limiter.lower_request_rate(failed_request).await?;
+        }
     }
 
     pub async fn fetch_url_body(&self, url: &str) -> anyhow::Result<String> {
